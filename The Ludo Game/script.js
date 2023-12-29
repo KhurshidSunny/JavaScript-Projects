1825e0abb8241578bf66c59a9d97b47f146bb10c
Khurshid Khan <khurshidsunny45@gmail.com>
2023-12-29 21:03:43 +0500

    implemented the New game button for reseting the game and completes the project

--- The Ludo Game/script.js
+++ The Ludo Game/script.js
@@ -13,13 +13,33 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const diceEl = document.querySelector('.dice');
 
-const scores = [90, 90];
-let activePlayer = 0;
-let currentScore = Number(current0El.textContent);
-score0El.textContent = 0;
-score1El.textContent = 0;
+let scores, activePlayer, currentScore, isPlaying;
 
-diceEl.classList.add('hidden');
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  isPlaying = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  // Remove the dice
+  diceEl.classList.add('hidden');
+
+  // remove the winner if there is
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  // make the player 1 active by default
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,42 +50,60 @@ function switchPlayer() {
   player1El.classList.toggle('player--active');
 }
 
+// New Game or reset the game funtion
+btnNew.addEventListener('click', init);
+
 // Roll dice function
 function rollDice() {
-  // 1: Generate a random number
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (isPlaying) {
+    // 1: Generate a random number
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // Display the Dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // Display the Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // Check for 1
-  if (dice !== 1) {
-    currentScore += dice;
-    // 2. select element dynamically
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch player
-    switchPlayer();
+    // Check for 1
+    if (dice !== 1) {
+      currentScore += dice;
+      // 2. select element dynamically
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch player
+      switchPlayer();
+    }
+  }
+}
+
+function holdScore() {
+  if (isPlaying) {
+    // 1: Add current score to the total score of the active player
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2: check if the palyer reaches 100 and wins
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      isPlaying = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+      // hide the dice
+      diceEl.classList.add('hidden');
+    } else {
+      // switch player
+      switchPlayer();
+    }
   }
 }
 
 btnRoll.addEventListener('click', rollDice);
 
-btnHold.addEventListener('click', function () {
-  // 1: Add current score to the total score of the active player
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-
-  // 2: check if the palyer reaches 100 and wins
-  if (scores[activePlayer] >= 100) {
-    document
-      .getElementById(`score--${activePlayer}`)
-      .classList.add('player--winner');
-  } else {
-    // switch player
-    switchPlayer();
-  }
-});
+btnHold.addEventListener('click', holdScore);
