7a8909bf489be47415ad1046b8a126baea87c9d6
Khurshid Khan <khurshidsunny45@gmail.com>
2024-01-31 01:07:23 +0500

    Adding a todo task to the app, funcationality is implemented

--- Todo List App/script.js
+++ Todo List App/script.js
@@ -1,12 +1,36 @@
 const taskInput = document.querySelector("#task-input");
+const taskDisplay = document.querySelectorAll(".task");
+const checker = document.querySelector(".checker");
 const addButton = document.querySelector(".add-button");
-const catCircleBusiness = document.querySelector(".cat1-circle");
-const catCirclePersonal = document.querySelector(".cat2-circle");
+const categoryContainer = document.querySelector(".category");
+
+const todoListContainer = document.querySelector(".todo-list");
+
+let catValue;
 
 addButton.addEventListener("click", function () {
-  console.log(taskInput.value);
+  const todoTask = `<div class="task-container">
+  <input class="checker-${catValue}"   type="radio" />
+  <input class="task" type="text"  value=${taskInput.value} />
+  <i class="fas fa-ellipsis-v menu-button"></i>
+</div>`;
+
+  todoListContainer.insertAdjacentHTML("afterbegin", todoTask);
+
+  taskInput.value = "";
 });
 
-catCircleBusiness.addEventListener("click", function () {
-  catCircleBusiness.style.backgroundColor = `rgb(235, 21, 168)`;
+// selecting the category
+categoryContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("cat-circle")) {
+    const { cat } = e.target.dataset;
+    catValue = cat;
+
+    document.querySelectorAll(".cat-circle").forEach(function (catEl) {
+      catEl.classList.remove(`filled-business`);
+      catEl.classList.remove("filled-personal");
+    });
+
+    e.target.classList.add(`filled-${cat}`);
+  }
 });
