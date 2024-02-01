648756c07d7a7e6ad12d70973ba42d7992ae9c02
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-01 23:01:46 +0500

    Added the menu button box and implemented the delete the task from the list

--- Todo List App/script.js
+++ Todo List App/script.js
@@ -5,20 +5,61 @@ const addButton = document.querySelector(".add-button");
 const categoryContainer = document.querySelector(".category");
 
 const todoListContainer = document.querySelector(".todo-list");
+const menuBox = document.querySelector(".menu-box");
 
 let catValue;
 let taskNo = 0;
+let allTasks = -1;
+
 addButton.addEventListener("click", function () {
-  const todoTask = `<div class="task-container">
+  if (!taskInput.value) return;
+  const todoTask = `<div class="task-container" data-task-no="${taskNo}">
   <div class="filled-${catValue} circle-task" data-task="${taskNo}"></div>
   <input data-desc="${taskNo}" class="task" type="text"  value="${taskInput.value}" />
-  <i class="fas fa-ellipsis-v menu-button"></i>
+  <i class="fas fa-ellipsis-v menu-button" data-button="${taskNo}"></i>
+  <div class="menu-box hide" >
+            <button class="delete-button" data-task="${taskNo}">Delete</button>
+            <button class="edit-button">Edit</button>
+          </div>
 </div>`;
 
   todoListContainer.insertAdjacentHTML("beforeend", todoTask);
 
   taskInput.value = "";
   taskNo++;
+  allTasks++;
+});
+
+// show menu button box
+
+todoListContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("menu-button")) {
+    const { button } = e.target.dataset;
+
+    // Get the position of the clicked button
+    const buttonRect = e.target.getBoundingClientRect();
+    // console.log(buttonRect);
+
+    // Position the menu box to the clicked button next
+    // menuBox.style.top = `${buttonRect.top}px`;
+    // menuBox.style.left = `${buttonRect.right}px`;
+
+    menuBox.classList.toggle("display-menu-box");
+  }
+});
+
+// delete a task
+
+menuBox.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete-button")) {
+    let curData;
+    const taskToDelete = document.querySelectorAll(".task-container");
+    // delete the selected element
+    taskToDelete[allTasks].remove();
+    // also hide the menu box
+    menuBox.classList.remove("display-menu-box");
+    allTasks--;
+  }
 });
 
 todoListContainer.addEventListener("click", function (e) {
