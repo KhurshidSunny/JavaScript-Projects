dab64160c38fd03489a117fc18fc97a3e09ee9f4
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-04 21:39:12 +0500

    Implemented the selection the correct and incorrect option with the styles applied, also implemented the reward for scoring the marks

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -6,13 +6,17 @@ const btnNext = document.querySelector(".next-btn");
 const rulesContainer = document.querySelector(".rules-section");
 const quizContainer = document.querySelector(".quiz-section");
 
-const allOptions = document.querySelectorAll(".options p");
+const allOptions = document.querySelectorAll(".option");
 const questionElement = document.querySelector(".question");
 const totalQElement = document.querySelectorAll(".total-question");
 const correctAnsElement = document.querySelector(".correct-answers");
 const curQuestionElement = document.querySelector(".current-question");
+const allCircles = document.querySelectorAll(".circle");
 
-let curQuestion = 0;
+const rewardContainer = document.querySelector(".reward-section");
+
+let curQuestion = 1;
+let correctAnswersDone = 0;
 
 const allQuestions = [
   {
@@ -24,7 +28,7 @@ const allQuestions = [
       optionC: "C) To remove an event listener",
       optionD: "D) To modify the event propagation",
     },
-    correctAnswer: "A",
+    correctAnswer: "A) To create a new event",
   },
 
   {
@@ -36,7 +40,7 @@ const allQuestions = [
       optionC: "C) undefined",
       optionD: "D) function",
     },
-    correctAnswer: "A",
+    correctAnswer: "A) object",
   },
 
   {
@@ -45,9 +49,9 @@ const allQuestions = [
       optionA: "A) var",
       optionB: "B) let",
       optionC: "C) const",
-      optionD: "D) all of the aboven",
+      optionD: "D) all of the above",
     },
-    correctAnswer: "D",
+    correctAnswer: "D) all of the above",
   },
 
   {
@@ -59,7 +63,7 @@ const allQuestions = [
       optionC: 'C) It declares a constant named "name"',
       optionD: "D) It is an error in JavaScript syntax",
     },
-    correctAnswer: "B",
+    correctAnswer: "B) It defines a function with a default parameter",
   },
 ];
 
@@ -71,36 +75,69 @@ btnContinue.addEventListener("click", function () {
   quizContainer.classList.remove("hidden");
 });
 
+// reseting all the options
+const resetOptions = function () {
+  allOptions.forEach((opt, i) => {
+    opt.classList.remove("option-green", "option-red");
+    allCircles[i].classList.remove("circle-green", "circle-red");
+  });
+};
+
 const optionsContainer = document.querySelector(".options");
-optionsContainer.addEventListener("click", function (e) {
+
+const optionSelection = function (e) {
   const event = e.target;
-  const onlyOption = optionsContainer.closest(".option");
+
   const optionElement = event.closest(".option");
   if (!optionElement) return;
   const { optionNo } = optionElement.dataset;
-});
+  const selectedOption = allOptions[optionNo].querySelector("p").textContent;
+  const correctAns = allQuestions[curQuestion - 1].correctAnswer;
+
+  if (correctAns === selectedOption) {
+    correctAnswersDone++;
+    console.log(correctAnswersDone);
+    optionElement.classList.add("option-green");
+    allCircles[optionNo].classList.add("circle-green");
+  } else {
+    allOptions.forEach((opt, i) => {
+      if (opt.querySelector("p").textContent === correctAns) {
+        opt.classList.add("option-green");
+        allCircles[i].classList.add("circle-green");
+      }
+      opt.classList.add("option-red");
+      allCircles[i].classList.add("circle-red");
+    });
+  }
+};
+optionsContainer.addEventListener("click", optionSelection);
 
 // initial values
-const initializer = function (nextQuestion) {
-  const {
-    question,
-    options: { optionA, optionB, optionC, optionD },
-    correctAnswer,
-  } = allQuestions[nextQuestion];
+const initializer = function (nextQuestion = 0) {
+  const { question, options, correctAnswer } = allQuestions[nextQuestion];
+  const optionsArray = Object.values(options);
+
+  allOptions.forEach((opt, i) => {
+    opt.querySelector("p").textContent = optionsArray[i];
+  });
 
   questionElement.textContent = question;
-  allOptions[0].textContent = optionA;
-  allOptions[1].textContent = optionB;
-  allOptions[2].textContent = optionC;
-  allOptions[3].textContent = optionD;
-  curQuestionElement.text = 35;
-  curQuestionElement.textContent = 2;
+
+  curQuestionElement.textContent = nextQuestion + 1;
   totalQElement.forEach((q) => (q.textContent = numOfQuestions));
 };
+initializer();
 
 const renderQuestion = function (nextQuestion) {
-  initializer(nextQuestion);
+  if (curQuestion == numOfQuestions) {
+    quizContainer.classList.add("hidden");
+    rewardContainer.classList.remove("hidden");
+    correctAnsElement.textContent = correctAnswersDone;
+  }
   curQuestion++;
+  if (curQuestion - 1 === numOfQuestions) return;
+  initializer(nextQuestion);
+  resetOptions();
 };
 
 // Next button handler
