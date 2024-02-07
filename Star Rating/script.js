5e68ec19ece5867fe507347dc4942dfd02a39ac0
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-07 12:33:18 +0500

    refactor the code

--- Star Rating/script.js
+++ Star Rating/script.js
@@ -10,53 +10,74 @@ const starsText = [
   "Excellent 😃",
 ];
 
-// applying event handler on all stars using Event Deligation
 let activeMouseover = false;
-const checkRating = function (no, color, event = false) {
+const giveRating = function (no, color, event) {
+  // showing each rating description
   starsText.forEach((st, i) => {
     if (no === i) {
-      if (color === "yellow") ratingTextEl.textContent = st;
-      else ratingTextEl.textContent = "Rate us 😍";
+      if (color === "yellow") {
+        ratingTextEl.textContent = st;
+      }
+      // showing the rating description
+      else if (color === "white" && activeMouseover) {
+        ratingTextEl.textContent = "Rate us 😍";
+      }
     }
   });
+
   for (let i = 0; i <= no; i++) {
+    // for clicking on the star
     if (color === "yellow" && event === "click") {
       stars[i].style.color = color;
+      ratingTextEl.textContent = starsText[i];
       activeMouseover = false;
-    } else if (color === "yellow" && event === "mouseover") {
+    }
+
+    // hovering mouse on star
+    else if (color === "yellow" && event === "mouseover") {
       stars[i].style.color = color;
       activeMouseover = true;
-    } else if (color === "white" && event === "mouseout" && activeMouseover) {
+    }
+
+    // for mouse out from star
+    else if (color === "white" && event === "mouseout" && activeMouseover) {
       stars[i].style.color = color;
     }
   }
 };
 
+// mouseover handler
+const mouseoverHandler = function (e) {
+  const event = e.target;
+  const star = Number(event.dataset.star);
+  if (event.classList.contains("icon")) {
+    giveRating(star, "yellow", "mouseover");
+  }
+};
+
+// mouseoutHanlder
+const mouseoutHandler = function (e) {
+  const event = e.target;
+  const star = Number(event.dataset.star);
+
+  if (event.classList.contains("icon")) {
+    giveRating(star, "white", "mouseout");
+  }
+};
+
+// click handler
+const clickHandler = function (e) {
+  const event = e.target;
+  const star = Number(event.dataset.star);
+
+  if (event.classList.contains("icon")) {
+    giveRating(star, "yellow", "click");
+  }
+};
+
 // hovering the mouse on star
-starContainer.addEventListener("mouseover", function (e) {
-  const event = e.target;
-  const star = Number(event.dataset.star);
-  if (event.classList.contains("icon")) {
-    checkRating(star, "yellow", "mouseover");
-  }
-});
-
+starContainer.addEventListener("mouseover", mouseoverHandler);
 // leaving the mouse from the star
-starContainer.addEventListener("mouseout", function (e) {
-  const event = e.target;
-  const star = Number(event.dataset.star);
-
-  if (event.classList.contains("icon")) {
-    checkRating(star, "white", "mouseout");
-  }
-});
-
-// click event on each star
-starContainer.addEventListener("click", function (e) {
-  const event = e.target;
-  const star = Number(event.dataset.star);
-
-  if (event.classList.contains("icon")) {
-    checkRating(star, "yellow", "click");
-  }
-});
+starContainer.addEventListener("mouseout", mouseoutHandler);
+// click  on each star
+starContainer.addEventListener("click", clickHandler);
