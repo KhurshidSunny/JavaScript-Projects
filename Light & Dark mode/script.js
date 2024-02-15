6a2af5e0e8ee76f77c3fa9cf433ff712ec148d2d
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-15 23:16:44 +0500

    Feature: implemented the toggle dark and light functionality

--- Light & Dark mode/script.js
+++ Light & Dark mode/script.js
@@ -1,6 +1,18 @@
 const toggleButton = document.querySelector(".toggle-button");
+const toggleBtnContainer = document.querySelector(".toggle-container");
+const rootStyles = getComputedStyle(document.documentElement);
+const primaryColor = rootStyles.getPropertyValue("--primary-color");
 
-toggleButton.addEventListener("click", function () {
-  toggleButton.classList.toggle("turn-on");
-  console.log("hello");
-});
+let flag = true;
+const toggle = function () {
+  if (flag) {
+    toggleButton.classList.add("turn-on");
+    toggleBtnContainer.style.backgroundColor = `${primaryColor}`;
+    flag = false;
+  } else {
+    toggleButton.classList.remove("turn-on");
+    toggleBtnContainer.style.backgroundColor = `rgb(203, 199, 199)`;
+    flag = true;
+  }
+};
+toggleButton.addEventListener("click", toggle.bind(flag));
