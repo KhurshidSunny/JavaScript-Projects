7d5f357be267f8e084d9b574fd9529944230f77d
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-16 01:22:53 +0500

    feature: implemented toggling between light and ark mode with changing the illustration colors

--- Light & Dark mode/script.js
+++ Light & Dark mode/script.js
@@ -3,15 +3,26 @@ const toggleBtnContainer = document.querySelector(".toggle-container");
 const rootStyles = getComputedStyle(document.documentElement);
 const primaryColor = rootStyles.getPropertyValue("--primary-color");
 
+const images = document.querySelectorAll(".image-container img");
+
 let flag = true;
 const toggle = function () {
   if (flag) {
     toggleButton.classList.add("turn-on");
     toggleBtnContainer.style.backgroundColor = `${primaryColor}`;
+
+    document.documentElement.setAttribute("data-theme", "dark");
+
+    images.forEach((img, i) => (img.src = `img/img-dark-${i}.svg`));
+
     flag = false;
   } else {
     toggleButton.classList.remove("turn-on");
     toggleBtnContainer.style.backgroundColor = `rgb(203, 199, 199)`;
+
+    document.documentElement.setAttribute("data-theme", "light");
+
+    images.forEach((img, i) => (img.src = `img/img-light-${i}.svg`));
     flag = true;
   }
 };
