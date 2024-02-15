2cb70c93f64db78688c911f4fb0a05b2487abb8b
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-16 02:05:10 +0500

    Code Refactor: Refactored the code by making some functional components

--- Light & Dark mode/script.js
+++ Light & Dark mode/script.js
@@ -1,29 +1,46 @@
+//  SELECTORS
 const toggleButton = document.querySelector(".toggle-button");
 const toggleBtnContainer = document.querySelector(".toggle-container");
 const rootStyles = getComputedStyle(document.documentElement);
 const primaryColor = rootStyles.getPropertyValue("--primary-color");
-
+const modeContainer = document.querySelector(".mode");
 const images = document.querySelectorAll(".image-container img");
+const navBar = document.querySelector(".nav-bar");
+const textBox = document.querySelector(".text-box");
+
+// FUNCTIONS
+const darkMode = function () {
+  toggleButton.classList.add("turn-on");
+  toggleBtnContainer.style.backgroundColor = `${primaryColor}`;
+  navBar.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = `rgb(0 0 0 / 50%)`;
+
+  document.documentElement.setAttribute("data-theme", "dark");
+
+  images.forEach((img, i) => (img.src = `img/img-dark-${i}.svg`));
+  modeContainer.children[0].textContent = "Dark Mode";
+  modeContainer.children[1].classList.add("fa-moon");
+};
+
+const lightMode = function () {
+  toggleButton.classList.remove("turn-on");
+  toggleBtnContainer.style.backgroundColor = `rgb(203, 199, 199)`;
+
+  document.documentElement.setAttribute("data-theme", "light");
+
+  images.forEach((img, i) => (img.src = `img/img-light-${i}.svg`));
+  modeContainer.children[0].textContent = "Light Mode";
+  modeContainer.children[1].classList.remove("fa-moon");
+};
 
 let flag = true;
 const toggle = function () {
   if (flag) {
-    toggleButton.classList.add("turn-on");
-    toggleBtnContainer.style.backgroundColor = `${primaryColor}`;
-
-    document.documentElement.setAttribute("data-theme", "dark");
-
-    images.forEach((img, i) => (img.src = `img/img-dark-${i}.svg`));
-
+    darkMode();
     flag = false;
   } else {
-    toggleButton.classList.remove("turn-on");
-    toggleBtnContainer.style.backgroundColor = `rgb(203, 199, 199)`;
-
-    document.documentElement.setAttribute("data-theme", "light");
-
-    images.forEach((img, i) => (img.src = `img/img-light-${i}.svg`));
     flag = true;
+    lightMode();
   }
 };
 toggleButton.addEventListener("click", toggle.bind(flag));
