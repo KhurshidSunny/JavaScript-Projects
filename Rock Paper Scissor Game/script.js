045d8aa8289ea3a45bf966e72353db53ab1abe3a
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-17 15:40:19 +0500

    Feature: Reseting the game and displaying player and computer score and options on the screen, functionalities are implemented

--- Rock Paper Scissor Game/script.js
+++ Rock Paper Scissor Game/script.js
@@ -1,8 +1,30 @@
 const playerIconContainer = document.querySelector(".player-icons");
 const computerIconsEl = document.querySelectorAll(".com-icon");
+const playerScoreEl = document.querySelector(".player-score");
+const playerOptEl = document.querySelector(".player-option");
+const comScoreEl = document.querySelector(".com-score");
+const comOptEl = document.querySelector(".com-option");
+const winText = document.querySelector(".win-text");
+const repeatBtn = document.querySelector(".repeat-btn");
 
 const gameOptions = ["rock", "paper", "scissor"];
+let comScore = 0;
+let playerScore = 0;
 
+const resetGame = function () {
+  playerScoreEl.textContent = "";
+  comScoreEl.textContent = "";
+  playerOptEl.textContent = "";
+  comOptEl.textContent = "";
+  winText.textContent = "";
+
+  playerIconContainer.querySelectorAll(".icon").forEach((opt, i) => {
+    opt.classList.remove("selected");
+    computerIconsEl[i].classList.remove("selected");
+  });
+};
+
+resetGame();
 const computerSelection = function () {
   const randInt = Math.floor(Math.random() * 3);
   const comOption = gameOptions[randInt];
@@ -28,12 +50,27 @@ playerIconContainer.addEventListener("click", function (e) {
     if (playerOption === "scissor") event.classList.add("selected");
 
     const computerOption = computerSelection();
-    if (playerOption === "rock" && computerOption === "scissor") {
+    if (playerOption === computerOption) {
+      winText.textContent = "It's Tie";
+    } else if (playerOption === "rock" && computerOption === "scissor") {
       event.classList.add("selected");
+      playerScore++;
     } else if (playerOption === "scissor" && computerOption === "paper") {
-      console.log("player win");
+      playerScore++;
+      winText.textContent = "You win";
     } else if (playerOption === "paper" && computerOption === "rock") {
-      console.log("player win");
-    } else console.log("computer win");
+      playerScore++;
+      winText.textContent = "You win";
+    } else {
+      comScore++;
+      winText.textContent = "Computer win";
+    }
+
+    playerScoreEl.textContent = playerScore;
+    comScoreEl.textContent = comScore;
+    playerOptEl.textContent = playerOption;
+    comOptEl.textContent = computerOption;
   }
 });
+
+repeatBtn.addEventListener("click", resetGame);
