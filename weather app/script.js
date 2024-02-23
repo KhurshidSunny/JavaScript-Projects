5e48392aadbd00e83159aa05711f273666990d06
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-23 23:08:13 +0500

    Render data on display and refactor the code

--- weather app/script.js
+++ weather app/script.js
@@ -1,50 +1,101 @@
 const searchPlaceEl = document.querySelector(".search-place");
 const searchBtn = document.querySelector(".search-icon");
 const weatherImgEl = document.querySelector(".weather-img");
+const temperatureEl = document.querySelector(".weather-deg");
+const humidityEl = document.querySelector(".humidity");
+const windSpeedEl = document.querySelector(".wind-speed");
+const countryNameEl = document.querySelector(".place");
+const weatherDescEl = document.querySelector(".weather-description");
 
-let cityName = "";
+let countryName = "";
 
-const renderWeatherImg = function () {
+const weatherCode = [
+  "01n",
+  "02n",
+  "03n",
+  "04n",
+  "09n",
+  "10n",
+  "11n",
+  "13n",
+  "50n",
+  "01d",
+  "02d",
+  "03d",
+  "04d",
+  "09d",
+  "10d",
+  "11d",
+  "13d",
+  "50d",
+];
+
+const renderWeatherImg = function (code) {
   return new Promise(function (resolve, reject) {
-    resolve(weatherImgEl.src);
+    const matchCode = weatherCode.find((cod) => cod === code);
+
+    if (matchCode) {
+      resolve(`weather/${matchCode}.png`);
+    } else {
+      reject("invalid code");
+    }
   });
 };
 
-const renderWeatherDetails = function (data) {
-  const countryName = data.name;
+const displayDetails = async function (data) {
   const countryWeather = data.weather[0];
-  const { _, gust, speed } = data.wind;
-  console.log(countryWeather);
+  const { description, icon } = countryWeather;
+  const { temp: kelvin, humidity } = data.main;
+  const { speed: windSpeed } = data.wind;
+  const celcius = Math.floor(kelvin - 273);
+  temperatureEl.textContent = `${celcius}°C`;
+  countryNameEl.textContent = countryName[0]
+    .toUpperCase()
+    .concat(countryName.slice(1));
+  humidityEl.textContent = `${humidity}%`;
+  windSpeedEl.textContent = `${windSpeed} km/h`;
+  weatherDescEl.textContent = description;
+
+  const imgUrl = await renderWeatherImg(icon);
+  weatherImgEl.src = imgUrl;
+};
+
+const renderWeatherDetails = function (data) {
+  displayDetails(data);
+};
+
+const getJSON = async function (url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 const getPosition = async function (country) {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-  const data = await res.json();
+  const data = await getJSON(`https://restcountries.com/v3.1/name/${country}`);
   return data[0];
 };
 
 const getWeatherData = async function (country) {
   const pos = await getPosition(country);
-  console.log(pos);
-  const [lat, lng] = pos.latlng;
-  console.log(lat, lng);
 
+  const [lat, lng] = pos.latlng;
   const apiKey = `49a192416257fecea9d2a35770e556c0`;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
+
+  const data = await getJSON(url);
   renderWeatherDetails(data);
-  console.log(data);
 };
 
+getWeatherData("pakistan");
+
 searchPlaceEl.addEventListener("input", function (e) {
-  cityName = searchPlaceEl.value;
+  countryName = searchPlaceEl.value;
 });
 
 searchBtn.addEventListener("click", function () {
-  getWeatherData(cityName);
+  getWeatherData(countryName);
 });
 
 window.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") getWeatherData(cityName);
+  if (e.key === "Enter") getWeatherData(countryName);
 });
