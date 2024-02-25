d0227ee0b952cbbbf72f62610bad510f54fd8ea3
Khurshid Khan <khurshidsunny45@gmail.com>
2024-02-25 09:16:37 +0500

    Implemented the infinite scroll and also setup the loader

--- infinite scroll/script.js
+++ infinite scroll/script.js
@@ -1,5 +1,6 @@
 const imgContainer = document.querySelector(".image-container");
 const loader = document.querySelector(".loader");
+
 const count = 5;
 let ready = false;
 let totalImage;
@@ -7,10 +8,10 @@ let loadedImages = 0;
 let photosArray = [];
 
 const imageLoaded = function () {
-  console.log("image loaded");
-  loadedImages++;
+  loadedImages = loadedImages + 1;
   if (loadedImages === totalImage) {
     ready = true;
+    loader.classList.add("hidden");
     console.log("ready", ready);
   }
 };
@@ -25,19 +26,23 @@ const displayPhotos = function (data) {
     alt="${img.alt_description}"
     title="${img.alt_description}"
   /> </a>`;
+
     imgContainer.insertAdjacentHTML("beforeend", html);
+
     const imgEl = document.querySelector(".img");
     imgEl.src = img.urls.regular;
     imgEl.alt = img.alt_description;
-
-    imgEl.addEventListener("load", imageLoaded);
+  });
+  const allImages = document.querySelectorAll(".img");
+  allImages.forEach((img) => {
+    img.addEventListener("load", imageLoaded);
   });
 };
 
 const getPhotos = async function () {
   const apiKey = "A0MmkuYvgCj7MmWs2a35NGxXbji7hEnK8arZryYaSFw";
   const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-  //   const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
   try {
     const response = await fetch(apiUrl);
     if (!response.ok) throw new Error("The photo can not be found");
@@ -56,15 +61,9 @@ getPhotos();
 window.addEventListener("scroll", function () {
   if (
     this.window.scrollY + this.window.innerHeight >=
-    this.document.body.offsetHeight
+      this.document.body.offsetHeight &&
+    ready
   ) {
-    console.log("scroll end");
+    getPhotos();
   }
 });
-
-// const c = async function () {
-//   const res = await fetch(apiUrl);
-//   const data = await res.json();
-//   console.log(data);
-// };
-// c();
